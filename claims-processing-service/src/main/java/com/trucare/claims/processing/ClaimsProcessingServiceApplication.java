c02c847a289eef725d3c716682c6338a3386465d
ram-mvrr <ram.mvrr@gmail.com>
2024-10-02 19:53:34 +0530

    initial commit for trucare application

---
+++ claims-processing-service/src/main/java/com/trucare/claims/processing/ClaimsProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trucare.claims.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimsProcessingServiceApplication.class, args);
+	}
+
+}
---
+++ claims-processing-service/src/test/java/com/trucare/claims/processing/ClaimsProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trucare.claims.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
